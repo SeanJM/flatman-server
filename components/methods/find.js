133b58df757e4b7a43805e4bfdf9ab894ec840b7
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-12 18:36:34 -0500

    refactored 'find'

--- components/methods/find.js
+++ components/methods/find.js
@@ -1,64 +1,44 @@
 const _ = require('lodash');
-const isComponent = require('../../predicates/isComponent');
-const isDomNode = require('../../predicates/isDomNode');
-const getSelectorObject = require('../../tools/getSelectorObject');
+const getSelectorGroup = require('../../tools/getSelectorGroup');
+const getDomNode = require('../../tools/getDomNode');
 
-function hasClass(elementClass, selectorClass) {
-  if (Array.isArray(selectorClass)) {
-    return _.intersection(elementClass.split(' '), selectorClass).length;
-  } else if (selectorClass) {
-    return selectorClass.test(elementClass);
-  }
-}
-
-function findStringSelector(selector) {
-  let selectorObject = getSelectorObject(selector);
+function findNodes(node, selector) {
   let found = [];
-
-  function matches(element) {
-    if (selectorObject.tagName) {
-      if (selectorObject.tagName !== element.tagName) {
-        return;
-      }
-    }
-
-    for (var k in selectorObject.attributes) {
-      if (k === 'className') {
-        if (!hasClass(element.attr('class'), selectorObject.attributes[k])) {
-          return;
-        }
-      } else if (selectorObject.attributes[k]) {
-        if (typeof selectorObject.attributes[k] === 'string') {
-          if (selectorObject.attributes[k] !== element.attributes[k]) {
-            return;
-          }
-        } else if (!selectorObject.attributes[k].test(element.attributes[k])) {
-          return;
-        }
-      }
-    }
-
-    found.push(element);
-  }
 
   function find(childNodes) {
     childNodes.forEach(function (element) {
-      if (isComponent(element)) {
-        matches(element.node.document);
-        find(element.node.document.childNodes);
-      } else if (isDomNode(element)) {
-        matches(element);
-        find(element.childNodes);
+      var node = getDomNode(element);
+
+      if (node.is(selector)) {
+        found.push(element);
       }
+
+      find(node.childNodes);
     });
   }
 
-  find(this.childNodes);
+  find(node.childNodes);
+
   return found;
+}
+
+function findStringSelector(selector) {
+  let selectorList = getSelectorGroup(selector);
+  let found = [ [ this ] ];
+
+  while (selectorList.length) {
+    found[found.length - 1].forEach(function (node) {
+      found.push(findNodes(node, selectorList[0]));
+    });
+    selectorList.shift();
+  }
+
+  return found.slice(-1)[0];
 }
 
 function findComponentSelector(selector) {
   let found = [];
+
   function find(childNodes) {
     childNodes.forEach(function (element) {
       if (element instanceof selector) {
@@ -67,7 +47,9 @@ function findComponentSelector(selector) {
       find(element.childNodes);
     });
   }
+
   find(this.childNodes);
+
   return found;
 }
 
