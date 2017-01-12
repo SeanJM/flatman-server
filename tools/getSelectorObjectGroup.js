ee9d682e380efc81feaf69090c445ce9f2b3c5bb
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-12 18:38:18 -0500

    Deprecated

--- tools/getSelectorObjectGroup.js
+++
@@ -1,6 +0,0 @@
-const getSelectorGroup = require('./getSelectorGroup');
-const getSelectorObject = require('./getSelectorObject');
-
-module.exports = function getSelectorObjectGroup(selector) {
-  return getSelectorGroup(selector).map(getSelectorObject);
-};
