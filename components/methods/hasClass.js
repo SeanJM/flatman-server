b7744f9c944b68b497ef5e28c95f7faf0965daa1
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-12 18:36:57 -0500

    hasClass will return an array when passed an array

--- components/methods/hasClass.js
+++ components/methods/hasClass.js
@@ -1,6 +1,16 @@
 module.exports = function hasClass(className) {
-  return this.attributes.className
-    .split(' ')
-    .map(a => a.trim())
-    .includes(className);
+  var $className = this.attributes.className;
+
+  function each(className) {
+    return $className
+      .split(' ')
+      .map(a => a.trim())
+      .includes(className);
+  }
+
+  if (Array.isArray(className)) {
+    return className.map(each);
+  } else {
+    return each(className);
+  }
 };
