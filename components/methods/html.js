c25f5841b774ff1465f827cebb56afc7f592b939
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-13 11:17:34 -0500

    added return value for 'html()'

--- components/methods/html.js
+++ components/methods/html.js
@@ -1,8 +1,8 @@
 module.exports = function html(value) {
+  const parse = require('../../tools/parse');
   if (typeof value === 'string' || typeof value === 'number') {
-    this.childNodes = [value.toString()];
-  } else {
-    throw new Error('Invalid type of argument for \'.html\'');
+    this.childNodes = parse(value).childNodes;
+    return this;
   }
-  return this;
+  return this.childNodes.map(a => a.toHtml()).join('\n');
 };
