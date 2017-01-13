6e704475580bd969464316af827c6f9926fb2ca8
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-13 12:02:19 -0500

    made is cover more cases

--- test/tests/is.js
+++ test/tests/is.js
@@ -4,9 +4,15 @@ module.exports = {
   name : 'is()',
   this() {
     var a = el('div', { className : 'test', id : 'my-id', dataTest : 'my-test' });
-    return a.is('.test#my-id[data-test="my-test"]');
+    var b = el('div', { className : 'test' });
+    var c = el('div', { id : 'test' });
+    return [
+      a.is('.test#my-id[data-test="my-test"]'),
+      b.is('.test'),
+      c.is('#test'),
+    ];
   },
-  isEqual() {
-    return true;
+  isDeepEqual() {
+    return [ true, true, true ];
   }
 };
