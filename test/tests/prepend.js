6faf888a32efe4910e17bd2c438e01e4215af8f6
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-13 12:02:27 -0500

    'prepend' test

---
+++ test/tests/prepend.js
@@ -0,0 +1,21 @@
+var el = require('../../flatman').el;
+
+module.exports = {
+  name : 'prepend()',
+  this() {
+    var a = el('div');
+    var b = el('div');
+    var c = el('div');
+
+    a.append([ b ]);
+    a.prepend([ c ]);
+
+    return [
+      a.childNodes[0] === c,
+      a.childNodes[1] === b
+    ];
+  },
+  isDeepEqual() {
+    return [ true, true ];
+  }
+};
