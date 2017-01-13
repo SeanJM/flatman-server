fe76a672779d0eb8e4c3b124f6b8a6e8ca186b05
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-13 11:37:46 -0500

    on test

---
+++ test/tests/on.js
@@ -0,0 +1,24 @@
+var el = require('../../flatman').el;
+
+module.exports = {
+  name : 'on(\'click\')',
+  this() {
+    var result = [];
+
+    function onClick() {
+      toggle = !toggle;
+    }
+
+    var toggle = false;
+    var a = el('div');
+
+    a.on('click', onClick);
+    a.trigger('click');
+    result.push(toggle);
+
+    return toggle;
+  },
+  isDeepEqual() {
+    return [ true ];
+  }
+};
---
+++ test/tests/onClick.js
@@ -0,0 +1,23 @@
+var el = require('../../flatman').el;
+
+module.exports = {
+  name : 'on { onClick }',
+  this() {
+    var result = [];
+
+    function onClick() {
+      toggle = !toggle;
+    }
+
+    var toggle = false;
+    var a = el('div', { onClick : onClick });
+
+    a.trigger('click');
+    result.push(toggle);
+
+    return toggle;
+  },
+  isDeepEqual() {
+    return [ true ];
+  }
+};
