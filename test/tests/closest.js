bbb056fff45fd72effba80604d7e86bf25331d82
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-13 12:01:37 -0500

    fixed test using 'class' instead of 'className'

--- test/tests/closest.js
+++ test/tests/closest.js
@@ -1,15 +1,16 @@
 var el = require('../../flatman').el;
 
-var a = el('div');
-var b = el('div', { class : 'test' });
-var c = el('div', { class : 'test' });
-var d = el('div', { class : 'test' });
-
-a.append([ b.append([ c.append([ d ]) ]) ]);
 
 module.exports = {
   name : 'closest()',
   this() {
+    var a = el('div');
+    var b = el('div', { className : 'test' });
+    var c = el('div', { className : 'test' });
+    var d = el('div', { className : 'test' });
+
+    a.append([ b.append([ c.append([ d ]) ]) ]);
+
     return d.closest('.test') === c;
   },
   isEqual() {
