a42f0acdda1c23b7ba6146e22c428d0903e3759f
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-13 11:16:59 -0500

    Added 'html' test

---
+++ test/tests/html.js
@@ -0,0 +1,13 @@
+var el = require('../../flatman').el;
+
+module.exports = {
+  name : 'html()',
+  this() {
+    var a = el('div');
+    a.html('<span></span>');
+    return [ a.childNodes[0].tagName, a.html() ];
+  },
+  isDeepEqual() {
+    return [ 'span', '<span></span>' ];
+  }
+};
