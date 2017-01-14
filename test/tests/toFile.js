9ee3886e20ce382d4b59b4121d977da4dadd7238
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-14 07:56:28 -0500

    tests for 'toFile' and 'toHtml'

---
+++ test/tests/toFile.js
@@ -0,0 +1,32 @@
+const el = require('../../flatman').el;
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+  name : 'toHtml()',
+  this() {
+    var a = el('div', [
+      el('div', { className : 'div-1' }, [
+        el('div', { className : 'div-1-1' }, [
+          el('span', { className : 'div-1-1-1' }, [
+            el('span', { className : 'div-1-1-1-1' })
+          ])
+        ]),
+        el('div', { className : 'div-1-2' })
+      ]),
+      el('div', { className : 'div-2' }, [
+        el('div', { className : 'div-2-1' }),
+        el('div', { className : 'div-2-2' })
+      ]),
+      el('div', { className : 'div-3' }, [
+        el('div', { className : 'div-3-1' }),
+        el('div', { className : 'div-3-2' })
+      ])
+    ]);
+
+    return a.toFile('./toFile.html');
+  },
+  isEqual() {
+    return fs.readFileSync(path.resolve('test/tests/toHtml.html'), 'utf8');
+  }
+};
