145d356ff552795ce54937f3cf44a5cc93342985
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-16 09:52:35 -0500

    html component

--- components/Html.js
+++ components/Html.js
@@ -1,21 +1,61 @@
 const el = require('../tools/el');
+const Component = require('flatman-component');
+const fs = require('fs');
 
-module.exports = class Html {
+Component.create('HTML', {
   constructor() {
-    this.node = {
-      document : el('html')
+    this.dict = {
+      script : [],
+      head : [],
+      body : [],
+      favicon : [],
+      css : []
     };
+    this.onHtml = this.onHtml.bind(this);
+  },
+  onHtml() {
+    var body = this.node.document.find('body')[0];
+    var head = this.node.document.find('head')[0];
 
-    this.node.document.append([
-      this.node.head = el('head'),
-      this.node.body = el('body')
-    ]);
-  }
-  
+    body.append([].concat(
+      this.dict.body,
+      this.dict.script
+    ));
+
+    head.append([].concat(
+      this.dict.favicon,
+      this.dict.css
+    ));
+  },
   render() {
+    return el('html', {
+      onHtml : this.onHtml
+    }, [
+      el('head', [
+        el('meta', { httpEquiv : 'X-UX-Compatible', content : 'IE=edge,chrome=1' }),
+        el('meta', { charset : 'UTF-8' })
+      ]),
+      el('body')
+    ]);
+  },
+  append(children) {
+    children.forEach((child) => {
+      if (child.tagName === 'script') {
+        this.dict.script.push(child);
+      } else {
+        this.dict.body.push(child);
+      }
+    });
+  },
+  toHtml() {
     return (
       '<!DOCTYPE HTML>\n' +
-      render(this.childNodes)
+      this.node.document.toHtml()
     );
+  },
+  toFile(filename) {
+    const value = this.toHtml();
+    fs.writeFileSync(filename, value);
+    return value;
   }
-};
+});
