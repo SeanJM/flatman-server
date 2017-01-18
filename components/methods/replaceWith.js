8f3d6b7069a060f9108a1805db5a6b971795aeb1
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-17 21:42:43 -0500

    added error message for when node has no parent

--- components/methods/replaceWith.js
+++ components/methods/replaceWith.js
@@ -1,5 +1,10 @@
 module.exports = function replaceWith(domNode) {
-  var index = this.parentNode.childNodes.indexOf(this);
-  this.parentNode.childNodes[index] = domNode;
-  return domNode;
+  var index;
+  if (this.parentNode) {
+    index = this.parentNode.childNodes.indexOf(this);
+    this.parentNode.childNodes[index] = domNode;
+    return domNode;
+  } else {
+    throw new Error('Cannot run \'DomNode.prototype.replaceWith\', node requires a parentNode.');
+  }
 };
