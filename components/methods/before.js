4613a93a657ea5e44cf75563fe4d3de2b4669008
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-20 07:38:02 -0500

    Fixed before not accepting an empty value

--- components/methods/before.js
+++ components/methods/before.js
@@ -1,6 +1,13 @@
 const getDomNode = require('../../tools/getDomNode');
 
 module.exports = function before(MaybeNode) {
-  this.parentNode.childNodes.splice(this.parentNode.childNodes.indexOf(this), 0, MaybeNode);
+  const childNodes = this.parentNode.childNodes;
+  const index = childNodes.indexOf(this);
+
+  if (typeof MaybeNode === 'undefined') {
+    return childNodes[index - 1];
+  }
+
+  childNodes.splice(index, 0, MaybeNode);
   return this;
 };
