1febe0f655835c7c15fb418e2e773820a5c1a91a
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-20 07:33:14 -0500

    shortened the 'before' function

--- components/methods/before.js
+++ components/methods/before.js
@@ -1,15 +1,6 @@
 const getDomNode = require('../../tools/getDomNode');
 
 module.exports = function before(MaybeNode) {
-  const index = this.parentNode.childNodes.indexOf(this);
-
-  if (Array.isArray(MaybeNode)) {
-    throw new Error('Invalid argument for \'.before\', method takes a valid Node or Component and not an Array.');
-  } else if (typeof MaybeNode === 'undefined') {
-    return this.parentNode.childNodes[index - 1];
-  }
-
-  this.parentNode.childNodes.splice(index, 0, MaybeNode);
-  getDomNode(MaybeNode).parentNode = this.parentNode;
+  this.parentNode.childNodes.splice(this.parentNode.childNodes.indexOf(this), 0, MaybeNode);
   return this;
 };
