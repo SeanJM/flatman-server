8aa043691589ffe340fdcb88421b84fc52d0db92
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-20 07:32:22 -0500

    adjusted test case for 'class array'

--- test/tests/attr_class_array.js
+++ test/tests/attr_class_array.js
@@ -4,10 +4,11 @@ module.exports = {
   name : 'attr() class array',
   this() {
     var a = el('div');
+    var b = el('div', { className : [ 'this' ].concat('array') });
     a.attr('class', [ 'this', 'array' ]);
-    return a.attr('class');
+    return [ a.attr('class'), b.attr('class') ];
   },
-  isEqual() {
-    return 'this array';
+  isDeepEqual() {
+    return [ 'this array', 'this array' ];
   }
 };
