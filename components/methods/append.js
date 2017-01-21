f00e9ff53c734ec6a5d174040ca99ac7d77ab41a
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-21 09:17:29 -0500

    cleaned

--- components/methods/append.js
+++ components/methods/append.js
@@ -1,18 +1,17 @@
 module.exports = function append(childNodes) {
-  const self = this;
+  const childNodesRoot = childNodes.map(a => a.getNode ? a.getNode() : a);
 
-  if (typeof this.childNodes === 'undefined') {
-    this.childNodes = [];
-  }
-
-  childNodes.forEach(function (child) {
-    child.parentNode = self;
+  childNodesRoot.forEach((child) => {
+    if (child && child.trigger) {
+      if (child.parentNode) {
+        child.remove();
+      }
+      child.parentNode = this
+    };
   });
 
-  if (arguments.length === 1 && Array.isArray(childNodes)) {
-    [].push.apply(this.childNodes, childNodes);
-  } else {
-    throw new Error('flatman: Invalid arguement for \'.append\', only a single array is allowed');
+  if (arguments.length === 1 && Array.isArray(childNodesRoot)) {
+    [].push.apply(this.childNodes, childNodesRoot);
   }
 
   return this;
