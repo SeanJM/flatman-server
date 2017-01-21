daee04b35be78eafd07ae2cb2820a5c41f8fb711
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-01-21 09:18:14 -0500

    made the function smaller

--- components/methods/find.js
+++ components/methods/find.js
@@ -1,61 +1,49 @@
 const getSelectorGroup = require('../../tools/getSelectorGroup');
 const getDomNode = require('../../tools/getDomNode');
 
-function findNodes(node, selector) {
+function findPredicate(predicate) {
   let found = [];
 
   function find(childNodes) {
-    childNodes.forEach(function (node) {
-      if (node.childNodes) {
-        if (node.is(selector)) {
-          found.push(node);
+    childNodes.forEach(function (element) {
+      if (element.children) {
+        if (predicate(element)) {
+          found.push(element);
         }
-        find(node.childNodes);
+        find(element.childNodes);
       }
     });
   }
 
-  find(node.childNodes);
-
+  find(this.childNodes);
   return found;
 }
 
 function findStringSelector(selector) {
-  let selectorList = getSelectorGroup(selector);
+  let list = getSelectorGroup(selector);
   let found = [ [ this ] ];
+  let self = this;
 
-  while (selectorList.length) {
-    found[found.length - 1].forEach(function (node) {
-      found.push(findNodes(node, selectorList[0]));
-    });
-    selectorList.shift();
+  function each (node) {
+    found.push(findPredicate.call(node, function (element) {
+      return element.is(list[0]);
+    }));
+  }
+
+  while (list.length) {
+    found[found.length - 1].forEach(each);
+    list.shift();
   }
 
   return found.slice(-1)[0];
 }
 
-function findComponentSelector(selector) {
-  let found = [];
-
-  function find(childNodes) {
-    childNodes.forEach(function (element) {
-      if (element instanceof selector) {
-        found.push(element);
-      }
-      find(element.childNodes);
-    });
-  }
-
-  find(this.childNodes);
-
-  return found;
-}
 
 module.exports = function find(selector) {
   if (typeof selector === 'string') {
     return findStringSelector.call(this, selector);
   } else if (typeof selector === 'function') {
-    return findComponentSelector.call(this, selector);
+    return findPredicate.call(this, selector);
   }
   throw new Error('Invalid selector for \'find\'');
 };
