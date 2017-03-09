3fe2997d336b822dad16e4dc4751659d4a16b1d3
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-03-09 12:28:08 -0500

    Added 'after' method

--- components/methods/after.js
+++ components/methods/after.js
@@ -3,11 +3,13 @@ module.exports = function after(maybeNode) {
 
   if (Array.isArray(maybeNode)) {
     maybeNode.forEach(a => after.call(this, a));
-  } else if (maybeNode) {
+  } else if (maybeNode && this.parentNode) {
     index = this.parentNode.childNodes.indexOf(this);
     this.parentNode.childNodes.splice(index + 1, 0, maybeNode);
     maybeNode.parentNode = this.parentNode;
     return this;
+  } else if (!this.parentNode) {
+    throw new Error('Invalid node, \"' + this.tagName + '\" does not have a parentNode.');
   }
 
   index = this.parentNode.childNodes.indexOf(this);
