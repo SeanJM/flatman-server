32bb9229e53148f718c1df3fdc549e315e51bb0e
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-05-13 11:27:15 -0400

    Added empty line

--- components/methods/toFile.js
+++ components/methods/toFile.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports = function toFile(filename) {
-  const value = this.toHtml();
+  const value = this.toHtml() + '\n';
   fs.writeFileSync(filename, value);
   return value;
 };
