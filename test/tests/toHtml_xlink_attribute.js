dc196473eb4f7e55597216dd1117fd08ee03c4d2
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-07-02 11:00:11 -0400

    Removed unused variables

--- test/tests/toHtml_xlink_attribute.js
+++ test/tests/toHtml_xlink_attribute.js
@@ -1,8 +1,4 @@
-const { el, parse } = require('../../index');
-const fs = require('fs');
-const path = require('path');
-
-const html = fs.readFileSync(path.resolve('test/tests/toHtml_svg.html'), 'utf8');
+const { el } = require('../../index');
 
 module.exports = {
   name : 'toHtml() (xlink:href)',
