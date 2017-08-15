33c6d023502e3c9557bf2d0c0e40533f1b5aef40
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-08-15 12:26:16 -0400

    Linted

--- tools/css.js
+++ tools/css.js
@@ -3,8 +3,8 @@ const el = require('./el');
 function css(file) {
   file = /css$/.test(file)
     ? file
-    : file + '.css';
-  return el('link', { rel : 'stylesheet', href : file });
+    : file + ".css";
+  return el("link", { rel : "stylesheet", href : file });
 }
 
 module.exports = css;
