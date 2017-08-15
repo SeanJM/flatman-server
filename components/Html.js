dbd613a5ac78dcb0172c1070c7120dc57ef9c261
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-08-15 12:24:29 -0400

    Linted

--- components/Html.js
+++ components/Html.js
@@ -1,8 +1,8 @@
-const el = require('../tools/el');
-const Component = require('flatman-component');
-const fs = require('fs');
+const el = require("../tools/el");
+const Component = require("flatman-component");
+const fs = require("fs");
 
-Component.create('HTML', {
+Component.create("HTML", {
   constructor(props) {
     this.props = {
       script : [],
@@ -14,19 +14,20 @@ Component.create('HTML', {
     };
     this.onHtml = this.onHtml.bind(this);
   },
+
   onHtml() {
-    var body = this.document.find('body')[0];
-    var head = this.document.find('head')[0];
+    var body = this.document.find("body")[0];
+    var head = this.document.find("head")[0];
 
     if (this.props.isMobile) {
       head.append([
-        el('meta', {
-          name : 'viewport',
+        el("meta", {
+          name : "viewport",
           content : [
-            'width=device-width initial-scale=1',
-            'maximum-scale=1',
-            'user-scalable=0'
-          ].join(', ')
+            "width=device-width initial-scale=1",
+            "maximum-scale=1",
+            "user-scalable=0"
+          ].join(", ")
         })
       ]);
     }
@@ -41,34 +42,38 @@ Component.create('HTML', {
       this.props.link
     ));
   },
+
   render() {
-    return el('html', {
+    return el("html", {
       onHtml : this.onHtml
     }, [
-      el('head', [
-        el('meta', { httpEquiv : 'X-UX-Compatible', content : 'IE=edge,chrome=1' }),
-        el('meta', { charset : 'UTF-8' })
+      el("head", [
+        el("meta", { httpEquiv : "X-UX-Compatible", content : "IE=edge,chrome=1" }),
+        el("meta", { charset : "UTF-8" })
       ]),
-      el('body')
+      el("body")
     ]);
   },
+
   append(children) {
     children.forEach((child) => {
-      if (child.tagName === 'link') {
+      if (child.tagName === "link") {
         this.props.link.push(child);
-      } else if (child.tagName === 'script') {
+      } else if (child.tagName === "script") {
         this.props.script.push(child);
       } else {
         this.props.body.push(child);
       }
     });
   },
+
   toHtml() {
     return (
-      '<!DOCTYPE HTML>\n' +
+      "<!DOCTYPE HTML>\n" +
       this.document.toHtml()
     );
   },
+
   toFile(filename) {
     const value = this.toHtml();
     fs.writeFileSync(filename, value);
