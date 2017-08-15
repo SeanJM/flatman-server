a93c2f7465e3805672b2beef776d2cd573d8e632
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-08-15 12:27:01 -0400

    Added component augmentation

---
+++ init/augmentComponent.js
@@ -0,0 +1,14 @@
+const Component = require("flatman-component");
+const toHtml = require("../components/methods/toHtml");
+
+Component.fn("toHtml", function () {
+  return toHtml.call(
+    {
+      tagName: this.document.tagName,
+      attributes: this.document.attributes,
+      trigger: this.trigger,
+      childNodes: this.childNodes,
+      parentNode: this.parentNode
+    }
+  );
+});
