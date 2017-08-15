98974d3314a70b1156084224a766fba60160afdc
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-08-15 14:01:47 -0400

    Added test for rendering HTML

---
+++ test/tests/el(HTML)_with_component.js
@@ -0,0 +1,44 @@
+const { el, Component } = require("../../index");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = {
+  name : "el(\'HTML\') with component",
+  this() {
+    Component.create("x",  {
+      render() {
+        return el("div", { className: "component" });
+      }
+    });
+
+    Component.create("y", {
+      toHtml() {
+        return this.document.toHtml();
+      },
+
+      render() {
+        return el("HTML", [
+          el("div"),
+          el("div", [
+            el("div")
+          ]),
+          el("div")
+        ]);
+      }
+    });
+
+    var a = el("y");
+
+    a.append([ el("x") ]);
+
+    console.log(a);
+
+    return a.toHtml();
+  },
+  isEqual() {
+    return fs.readFileSync(
+      path.resolve("test/assets/el(HTML)_with_component.html"),
+      "utf8"
+    );
+  }
+};
