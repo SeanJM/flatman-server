8bac57411f934b12d7ef57fcb8e2484bcec35597
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-12-30 20:03:13 -0500

    Added error message for 'after' method

--- class/El/El.after.js
+++ class/El/El.after.js
@@ -1,6 +1,13 @@
 module.exports = function after(target) {
-  const parentNode = target.getNode().parentNode;
-  const index      = parentNode.childNodes.indexOf(target);
-  parentNode.childNodes.splice(index + 1, 0, this);
+  const targetNode = target.getNode();
+  const parentNode = targetNode.parentNode;
+  const index      = parentNode && parentNode.childNodes.indexOf(target);
+
+  if (typeof index === "number") {
+    parentNode.childNodes.splice(index + 1, 0, this);
+  } else {
+    throw "Cannot insert node after " + targetNode.tagName + ", target does have a parent";
+  }
+
   return this;
 };
