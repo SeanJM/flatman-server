753e0659db314724e39d214c94e23849ea551b41
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-12-30 20:20:55 -0500

    Fixed components not being appended at correct index

--- class/El/El.after.js
+++ class/El/El.after.js
@@ -1,7 +1,7 @@
 module.exports = function after(target) {
   const targetNode = target.getNode();
   const parentNode = targetNode.parentNode;
-  const index      = parentNode && parentNode.childNodes.indexOf(target);
+  const index      = parentNode && parentNode.childNodes.indexOf(targetNode);
 
   if (typeof index === "number") {
     parentNode.childNodes.splice(index + 1, 0, this);
--- test/tests/after.js
+++ test/tests/after.js
@@ -1,8 +1,8 @@
 const el = require("../../index");
 
 el.create("xd", {
-  render() {
-    return el();
+  render(props) {
+    return el({ class : props.class });
   }
 });
 
@@ -14,17 +14,24 @@ module.exports = {
     var c = el({ class : "c" });
     var d = el({ class : "d" });
     var f = el({ class : "f" });
+
     var e = el("xd");
     var g = el({ class : "g" });
     var h = el({ class : "h" });
 
+    var i = el("xd", { class : "i" });
+    var j = el("xd", { class : "j" });
+    var k = el("xd", { class : "k" });
+
     c.append([a, d]);
     e.append([ f, h ]);
+    j.append(i);
 
     b.after(a);
     g.after(f);
+    k.after(i);
 
-    return c.childNodes[1] === b && e.children()[1] === g;
+    return c.childNodes[1] === b && e.children()[1] === g && j.children()[1] === k.getNode();
   },
   isEqual() {
     return true;
