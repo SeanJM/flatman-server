231dee2e4fcf4283e79c8b99834e4832cb66d77f
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-12-30 19:14:24 -0500

    Fixed component 'after' not working

--- class/El/El.after.js
+++ class/El/El.after.js
@@ -1,5 +1,6 @@
 module.exports = function after(target) {
-  var index = target.parentNode.childNodes.indexOf(target);
-  target.parentNode.childNodes.splice(index + 1, 0, this);
+  const parentNode = target.getNode().parentNode;
+  const index      = parentNode.childNodes.indexOf(target);
+  parentNode.childNodes.splice(index + 1, 0, this);
   return this;
 };
--- test/tests/after.js
+++ test/tests/after.js
@@ -1,5 +1,11 @@
 const el = require("../../index");
 
+el.create("xd", {
+  render() {
+    return el();
+  }
+});
+
 module.exports = {
   name : "after()",
   this() {
@@ -7,10 +13,18 @@ module.exports = {
     var b = el({ class : "b" });
     var c = el({ class : "c" });
     var d = el({ class : "d" });
+    var f = el({ class : "f" });
+    var e = el("xd");
+    var g = el({ class : "g" });
+    var h = el({ class : "h" });
+
     c.append([a, d]);
+    e.append([ f, h ]);
+
     b.after(a);
-    console.log(c.childNodes[1]);
-    return c.childNodes[1] === b;
+    g.after(f);
+
+    return c.childNodes[1] === b && e.children()[1] === g;
   },
   isEqual() {
     return true;
