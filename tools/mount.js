3f7cb3d952bc85dce3e8972d22d4fd83b0b45a25
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-01-19 17:09:20 -0500

    made mount faster

--- tools/mount.js
+++ tools/mount.js
@@ -1,20 +1,21 @@
 const { MOUNTED } = require("./CONSTANTS");
 
-module.exports = function mount(node) {
+module.exports = function mount(node, shouldMount) {
   const children = node.childNodes;
   let p          = node.parentNode;
 
-  while (p && p.parentNode) {
-    p = p.parentNode;
+  if (typeof shouldMount === "undefined") {
+    while (p && p.parentNode) {
+      p = p.parentNode;
+    }
+    shouldMount = p && p.tagName === "html";
   }
 
-  if (children && p && p.tagName === "html" && MOUNTED.indexOf(node) === -1) {
+  if (children && shouldMount && MOUNTED.indexOf(node) === -1) {
     MOUNTED.push(node);
-
     node.trigger("mount");
-
     for (var i = 0, n = children.length; i < n; i++) {
-      mount(children[i]);
+      mount(children[i], shouldMount);
     }
   }
 };
