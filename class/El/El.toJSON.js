f2ab0a3c24b68ad3f6a5347c182465f946974888
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-02-14 14:26:26 -0500

    Changed id characters

--- class/El/El.toJSON.js
+++ class/El/El.toJSON.js
@@ -1,6 +1,18 @@
+const ALPHA       = "0987654321ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const alphaLength = ALPHA.length;
+
 module.exports = function toJSON() {
+  let key = "";
+  let i   = -1;
+  let n   = 10;
+
+  while (++i < n) {
+    key += ALPHA[Math.floor(Math.random() * alphaLength)];
+  }
+
   return {
     tagName    : this.tagName,
+    key        : key,
     attributes : this.attributes,
     childNodes : this.childNodes
   };
