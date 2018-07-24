8bbc16f764d06c6aa0eab0fbb63bda2db16d7c3b
Sean MacIsaac <seanjmacisaac@gmail.com>
2018-07-24 15:47:13 -0400

    Added error message when the component returns nothing.

--- src/tools/create-component.js
+++ src/tools/create-component.js
@@ -5,40 +5,44 @@ export function createComponent(maybeConstructor, props, children) {
     ? new maybeConstructor(props)
     : maybeConstructor(props);
 
-  if (isConstructor) {
-    component.tagName = maybeConstructor;
+  if (component) {
+    if (isConstructor) {
+      component.tagName = maybeConstructor;
 
-    if (maybeConstructor.prototype.render) {
-      component.node = maybeConstructor.prototype.render.call(component, props);
-      component.ref = component.ref || component.node.ref;
+      if (maybeConstructor.prototype.render) {
+        component.node = maybeConstructor.prototype.render.call(component, props);
+        component.ref = component.ref || component.node.ref;
 
-      if (typeof component.node === "undefined") {
-        throw new Error("Component does not return a valid element.");
-      }
-    }
-  }
-
-  if (component.node) {
-    component.getNode().on("mount", function () {
-      component.onMount && component.onMount({
-        target: component.getNode()
-      });
-    });
-
-    component.getNode().on("unmount", function () {
-      component.onUnmount && component.onUnmount({
-        target: component.getNode()
-      });
-    });
-
-    for (var k in component.node.refs) {
-      if (!component.refs[k]) {
-        component.refs[k] = component.node.refs[k];
+        if (typeof component.node === "undefined") {
+          throw new Error("Component does not return a valid element.");
+        }
       }
     }
 
-    component.append(children);
-  }
+    if (component.node) {
+      component.getNode().on("mount", function () {
+        component.onMount && component.onMount({
+          target: component.getNode()
+        });
+      });
 
-  return component;
+      component.getNode().on("unmount", function () {
+        component.onUnmount && component.onUnmount({
+          target: component.getNode()
+        });
+      });
+
+      for (var k in component.node.refs) {
+        if (!component.refs[k]) {
+          component.refs[k] = component.node.refs[k];
+        }
+      }
+
+      component.append(children);
+    }
+
+    return component;
+  } else {
+    throw new Error("Invalid component, a component cannot return nothing.");
+  }
 }
